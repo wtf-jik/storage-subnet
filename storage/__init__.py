d24b0a6de572a698404b2f577fe7dc9267d498ae
ifrit98 <ifrit98@gmail.com>
2024-01-18 07:15:17 +0000

    version code update

--- storage/__init__.py
+++ storage/__init__.py
@@ -51,7 +51,7 @@ class StorageVersion:
         )
 
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 version = StorageVersion.from_string(__version__)
 __spec_version__ = version.to_spec_version()
 
