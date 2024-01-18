01b41dfa83c32ae425f8be385748acc390ed2c89
ifrit98 <ifrit98@gmail.com>
2024-01-18 22:09:42 +0000

    hotfix validator rebalance in separate process call from subscription handler

--- storage/__init__.py
+++ storage/__init__.py
@@ -51,7 +51,7 @@ class StorageVersion:
         )
 
 
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 version = StorageVersion.from_string(__version__)
 __spec_version__ = version.to_spec_version()
 
